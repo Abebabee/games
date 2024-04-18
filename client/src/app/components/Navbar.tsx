6d1ac23a317045c6aac735fcad22d5628b4cdd06
Albin Fernström <albinfernstrom@hotmail.se>
2024-04-18 16:44:56 +0200

    feat: add link to logo

--- client/src/app/components/Navbar.tsx
+++ client/src/app/components/Navbar.tsx
@@ -1,14 +1,15 @@
+import Link from "next/link";
 import ThemeToggle from "./ThemeToggle";
 
 const Navbar = () => {
   return (
     <div className="bg-accent flex items-center justify-between min-h-16">
-        <div>
-            <h1 className="text-primary text-lg font-bold pl-5">Gamedle</h1>
-        </div>
-        <div className="flex items-center pr-5">
-          <ThemeToggle />
-        </div>
+      <Link href="/">
+        <h1 className="text-primary text-lg font-bold pl-5">Gamedle</h1>
+      </Link>
+      <div className="flex items-center pr-5">
+        <ThemeToggle />
+      </div>
     </div>
   );
 };
