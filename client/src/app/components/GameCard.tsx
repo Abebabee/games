5bb0a13dbbd736efba728740c3adfc5084a3161e
Albin Fernström <albinfernstrom@hotmail.se>
2024-04-18 17:16:51 +0200

    feat:add highscore and fix responsiveness of gamecard grid

--- client/src/app/components/GameCard.tsx
+++ client/src/app/components/GameCard.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Button from "./Button";
 
 interface GameCardProps {
@@ -13,20 +14,21 @@ const GameCard = ({ gameName, imageUrl, description, gameUrl }: GameCardProps) =
       <img
         alt={gameName}
         className="h-60 w-full object-cover"
-        height="300"
         src={imageUrl}
         style={{
           aspectRatio: "400/300",
           objectFit: "cover",
         }}
-        width="400"
       />
-      <div className="flex flex-col justify-center items-center p-3 text-center">
-        <div className="text-lg font-bold">{gameName}</div>
-        <div className="text-muted_foreground">{description}</div>
-      </div>
-      <div className="flex justify-end mr-2 mb-2">
-        <Button name="Play" href={gameUrl}/>
+      <div className="p-3">
+        <div className="text-lg font-bold text-center">{gameName}</div>
+        <div className="text-muted_foreground text-center">{description}</div>
+        <div className="flex justify-between mt-4">
+          <div className="text-center">
+            <p>Highscore: <span className="text-primary">0</span></p>
+          </div>
+          <Button name="Play" href={gameUrl}/>
+        </div>
       </div>
     </div>
   );
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,41 +1,46 @@
 import GameCard from "./components/GameCard";
 
-const index = () => {
+const IndexPage = () => {
   return (
-    <section className="grid grid-cols-1 content-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6 md:p-8 lg:p-10 bg-background h-[calc(100vh-4rem)]">
-      <GameCard
-        gameName={"Higher/Lower"}
-        gameUrl="/higherlower"
-        imageUrl={"https://placehold.co/600x400"}
-        description={
-          "Guess if country has higher or lower population than the other country"
-        }
-      />
-      <GameCard
-        gameName={"Game 2"}
-        gameUrl="/higherlower"
-        imageUrl={"https://placehold.co/200x400"}
-        description={
-          "Guess if country has higher or lower population than the other country"
-        }
-      />
-      <GameCard
-        gameName={"Game 3"}
-        gameUrl="/higherlower"
-        imageUrl={"https://placehold.co/400x400"}
-        description={
-          "Guess if country has higher or lower population than the other country"
-        }
-      />
-      <GameCard
-        gameName={"Game 4"}
-        gameUrl="/higherlower"
-        imageUrl={"https://placehold.co/400x400"}
-        description={
-          "Guess if country has higher or lower population than the other country"
-        }
-      />
+    <section className="flex justify-center items-center min-h-[calc(100vh-4rem)] p-6 bg-background">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          <GameCard
+            gameName={"Higher/Lower"}
+            gameUrl="/higherlower"
+            imageUrl={"https://placehold.co/600x400"}
+            description={
+              "Guess if country has higher or lower population than the other country"
+            }
+          />
+          <GameCard
+            gameName={"Game 2"}
+            gameUrl="/higherlower"
+            imageUrl={"https://placehold.co/200x400"}
+            description={
+              "Guess if country has higher or lower population than the other country"
+            }
+          />
+          <GameCard
+            gameName={"Game 3"}
+            gameUrl="/higherlower"
+            imageUrl={"https://placehold.co/400x400"}
+            description={
+              "Guess if country has higher or lower population than the other country"
+            }
+          />
+          <GameCard
+            gameName={"Game 4"}
+            gameUrl="/higherlower"
+            imageUrl={"https://placehold.co/400x400"}
+            description={
+              "Guess if country has higher or lower population than the other country"
+            }
+          />
+        </div>
+      </div>
     </section>
   );
 };
-export default index;
+
+export default IndexPage;
