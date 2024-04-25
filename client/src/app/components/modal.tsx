9cb5c6ed188261eba3ffa968cd1dd13ce3c6ec0c
Albin Fernström <albinfernstrom@hotmail.se>
2024-04-25 14:30:59 +0200

    feat: add modal to higher/lower game and optional button on modal

--- client/src/app/components/modal.tsx
+++ client/src/app/components/modal.tsx
@@ -1,23 +1,26 @@
+import Button from "./Button";
+
 interface ModalProps {
   header: string;
   text: string;
   color: string;
   onClose: () => void;
   score?: number;
+  buttonText?: string;
 }
-const Modal = ({ header, text, color, onClose, score }: ModalProps) => {
+const Modal = ({ header, text, color, onClose, score, buttonText}: ModalProps) => {
   return (
     <div
       className="fixed top-0 left-0 w-full h-full flex justify-center backdrop-blur-sm items-center z-20 text-foreground"
       onClick={onClose}
     >
       <div
-        className={`p-6 flex flex-col justify-center items-center text-center rounded-lg shadow-lg ${color}`}
+        className={`p-6 flex flex-col justify-center items-center text-center rounded-lg shadow-lg border-2 border-border ${color}`}
       >
         <p className="text-2xl font-semibold">{header}</p>
-        <p className="text-secondary_foreground">{text}</p>
-        {score && <div>
-          Score: {score}</div>}
+        <p className="text-secondary_foreground pb-2">{text}</p>
+        {score && <div>Score: {score}</div>}
+        {buttonText && (<Button name={buttonText} href={""} onClick={onClose}></Button>)}
       </div>
     </div>
   );
--- client/src/app/higherlower/page.tsx
+++ client/src/app/higherlower/page.tsx
@@ -119,6 +119,7 @@ const HigherLower = () => {
           text={gameoverMsg}
           color={bg}
           onClose={handleCloseModal}
+          buttonText="New game"
         ></Modal>
       )}
       <div className="absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-4 bg-accent rounded-full border-2 border-primary">
